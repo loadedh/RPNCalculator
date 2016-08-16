fbbb475271599671ad49f67552ad46b53147d036
Kieron Hagg <loadedh@gmail.com>
2016-08-16 17:37:41 +0100

    Add files via upload
    Changes wew

--- Random.js
+++ Random.js
@@ -2,35 +2,64 @@
 let rpn = (input) => {
     let inputArgs = input.split("");
 
-    let result = calculation(inputArgs);
+    let result = calculation(inputArgs); //Give the calculation our input and set it to "result" variable
 
-    return result.pop();
+    console.log(result);
+    return result;
 };
 
+const myMethods = {
+      '+'(a) {
+          return b => a + b;
+      },
+    '-'(a, b) {
+        return a - b;
+    },
+    '/'(a, b) {
+        return a / b;
+    },
+   '*'(a, b) {
+        return a * b;
+    },
+};
+
+const myMethods = new Map([
+   ['*', mult],
+]);
+
+myMethods.get('*');
+
+const withA = myMethods[operator](a);
+a(b) === a + b;
+
 let calculation = (input) => {
-    let inputArgs = input, resultArr = [], i;
+    let result = 0;
 
     let arLoop = () => {
+        let inputArgs = input, values = [], i;
         i = inputArgs.shift();
+
         if (isFinite(inputArgs[i])) {
-            resultArr.push(inputArgs[i])
+            values.push(inputArgs[i])
         } else {
-            let number1 = resultArr.pop(), number2 = resultArr.pop();
+            let number1 = values.pop(), number2 = values.pop();
 
             //Check what operator was used and apply the correct formula
             if (inputArgs[i] === "+") {
-                resultArr.push(parseInt(number1) + parseInt(number2));
+                result = number1 + number2;
             } else if (inputArgs[i] === "-") {
-                resultArr.push(parseInt(number1) - parseInt(number2));
+                result = number1 - number2;
             } else if (inputArgs[i] === "*") {
-                resultArr.push(parseInt(number1) * parseInt(number2));
+                result = number1 * number2;
             } else if (inputArgs[i] === "/") {
-                resultArr.push(parseInt(number1) / parseInt(number2));
+                result = number1 / number2;
             }
-        }
-        if (resultArr.length > 1) {
+        } if (values.length > 1) {
             return arLoop();
+        } else if (isNaN(result) || result === "undefined") {
+            console.log("Ohh you fucked up!")
+        } else {
+            return result;
         }
-        return resultArr;
     }
 };
